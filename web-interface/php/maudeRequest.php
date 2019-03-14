29843d784ced5da2f1803a1d5e3188521d42de8f
GlauberC <glaubercarv@gmail.com>
2019-03-14 10:22:30 -0300

    Some minors fixes

--- web-interface/php/maudeRequest.php
+++ web-interface/php/maudeRequest.php
@@ -20,22 +20,21 @@
         if(preg_match('/then.+/i', $input[1])){
             $ask = '';
             $then = '';
+            $askRaw = '';
             preg_match('/then.+/i', $input[1], $then);
             $then = str_replace('then', '', $then[0]);
-            preg_match('/ask \'\w+\s*/i', $input[1], $ask);
-            preg_match("/\'\w+/i", $ask[0], $ask);
-            $ask = $ask[0];
+            preg_match('/ask \'\w+\s*/i', $input[1], $askRaw);
+            preg_match("/\'\w+/i", $askRaw[0], $ask);
 
-            $subs = "('C:Constraint <- upTerm($ask) ; ('P:Process <- upTerm($then))";
+            $subs = "('C:Constraint <- upTerm($ask[0]) ; ('P:Process <- upTerm($then))";
             $command = "($input[0]), 'askthen, $subs )";
             $command = escapeshellarg($command);
             $result =  shell_exec("timeout 30 sh -c \"echo red metaApp\($command \) . | $DIR_MAUDE $DIR_FILE_MAUDE $MODF 2>&1 \"");
         }else{
             $tell = '';
             preg_match("/\'\w+/i", $input[1], $tell);
-            $tell = $tell[0];
-
-            $subs = "( 'C:Constraint  <- upTerm($tell )";
+            
+            $subs = "( 'C:Constraint  <- upTerm($tell[0] )";
             $command = "($input[0]), 'tell, $subs )";
             $command = escapeshellarg($command);
             $result =  shell_exec("timeout 30 sh -c \"echo red metaApp\($command \) . | $DIR_MAUDE $DIR_FILE_MAUDE $MODF 2>&1 \"");
